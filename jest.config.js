040d769b4b068ea151707ac25889b49a695f1084
Mauricio Palma <mauricio.palma@woodlikeocean.com>
2020-01-14 14:37:18 +0100

    feat(mcq): initial setup

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+const TEST_REGEX = '(/__tests__/.*|(\\.|/)test)\\.(tsx?|ts?)$'
+
+module.exports = {
+  preset: 'ts-jest',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
+  testEnvironment: 'jsdom',
+  transform: {
+    '.(ts|tsx)': 'ts-jest',
+  },
+  testRegex: TEST_REGEX,
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/']
+}
